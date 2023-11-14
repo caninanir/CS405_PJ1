adfd5cbd20fab3fefe447accf7c34ff54d62823e
caninanir <129761848+caninanir@users.noreply.github.com>
2023-11-14 11:27:33 +0300

    Add files via upload

---
+++ project1-chatgpt-animate/utils.js
@@ -0,0 +1,239 @@
+function multiplyMatrices(matrixA, matrixB) {
+    var result = [];
+
+    for (var i = 0; i < 4; i++) {
+        result[i] = [];
+        for (var j = 0; j < 4; j++) {
+            var sum = 0;
+            for (var k = 0; k < 4; k++) {
+                sum += matrixA[i * 4 + k] * matrixB[k * 4 + j];
+            }
+            result[i][j] = sum;
+        }
+    }
+
+    // Flatten the result array
+    return result.reduce((a, b) => a.concat(b), []);
+}
+function createIdentityMatrix() {
+    return new Float32Array([
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1
+    ]);
+}
+function createScaleMatrix(scale_x, scale_y, scale_z) {
+    return new Float32Array([
+        scale_x, 0, 0, 0,
+        0, scale_y, 0, 0,
+        0, 0, scale_z, 0,
+        0, 0, 0, 1
+    ]);
+}
+
+function createTranslationMatrix(x_amount, y_amount, z_amount) {
+    return new Float32Array([
+        1, 0, 0, x_amount,
+        0, 1, 0, y_amount,
+        0, 0, 1, z_amount,
+        0, 0, 0, 1
+    ]);
+}
+
+function createRotationMatrix_Z(radian) {
+    return new Float32Array([
+        Math.cos(radian), -Math.sin(radian), 0, 0,
+        Math.sin(radian), Math.cos(radian), 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1
+    ])
+}
+
+function createRotationMatrix_X(radian) {
+    return new Float32Array([
+        1, 0, 0, 0,
+        0, Math.cos(radian), -Math.sin(radian), 0,
+        0, Math.sin(radian), Math.cos(radian), 0,
+        0, 0, 0, 1
+    ])
+}
+
+function createRotationMatrix_Y(radian) {
+    return new Float32Array([
+        Math.cos(radian), 0, Math.sin(radian), 0,
+        0, 1, 0, 0,
+        -Math.sin(radian), 0, Math.cos(radian), 0,
+        0, 0, 0, 1
+    ])
+}
+
+function getTransposeMatrix(matrix) {
+    return new Float32Array([
+        matrix[0], matrix[4], matrix[8], matrix[12],
+        matrix[1], matrix[5], matrix[9], matrix[13],
+        matrix[2], matrix[6], matrix[10], matrix[14],
+        matrix[3], matrix[7], matrix[11], matrix[15]
+    ]);
+}
+
+const vertexShaderSource = `
+attribute vec3 position;
+attribute vec3 normal; // Normal vector for lighting
+
+uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
+uniform mat4 normalMatrix;
+
+uniform vec3 lightDirection;
+
+varying vec3 vNormal;
+varying vec3 vLightDirection;
+
+void main() {
+    vNormal = vec3(normalMatrix * vec4(normal, 0.0));
+    vLightDirection = lightDirection;
+
+    gl_Position = vec4(position, 1.0) * projectionMatrix * modelViewMatrix; 
+}
+
+`
+
+const fragmentShaderSource = `
+precision mediump float;
+
+uniform vec3 ambientColor;
+uniform vec3 diffuseColor;
+uniform vec3 specularColor;
+uniform float shininess;
+
+varying vec3 vNormal;
+varying vec3 vLightDirection;
+
+void main() {
+    vec3 normal = normalize(vNormal);
+    vec3 lightDir = normalize(vLightDirection);
+    
+    // Ambient component
+    vec3 ambient = ambientColor;
+
+    // Diffuse component
+    float diff = max(dot(normal, lightDir), 0.0);
+    vec3 diffuse = diff * diffuseColor;
+
+    // Specular component (view-dependent)
+    vec3 viewDir = vec3(0.0, 0.0, 1.0); // Assuming the view direction is along the z-axis
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), shininess);
+    vec3 specular = spec * specularColor;
+
+    gl_FragColor = vec4(ambient + diffuse + specular, 1.0);
+}
+
+`
+
+/**
+ * @WARNING DO NOT CHANGE ANYTHING ABOVE THIS LINE
+ */
+
+
+
+/**
+ * 
+ * @TASK1 Calculate the model view matrix by using the chatGPT
+ */
+//can.inanir
+// https://chat.openai.com/share/5ffd8db3-5eea-4bb2-b45e-10a9997b3a9b
+function getChatGPTModelViewMatrix() {
+    const transformationMatrix = new Float32Array([
+        0.3535533845424652, 0.612372398853302, -0.7071067690849304, 0,
+        -0.612372398853302, 0.3535533845424652, -0.7071067690849304, 0,
+        -0.7071067690849304, -0.7071067690849304, -0, 0,
+        0.3, -0.25, 0, 1
+    ]);
+    return getTransposeMatrix(transformationMatrix);
+}
+
+
+/**
+ * 
+ * @TASK2 Calculate the model view matrix by using the given 
+ * transformation methods and required transformation parameters
+ * stated in transformation-prompt.txt
+ */
+//can.inanir
+// calculated using js file
+function getModelViewMatrix() {
+    const transformationMatrix = new Float32Array([
+        0.17, -0.28, 0.73, 0.30,
+        0.30, 0.36, 0.28, -0.25,
+       -0.35,  0.17, 0.61, 0, 
+       0, 0, 0, 1
+    ]);
+    return getTransposeMatrix(transformationMatrix);
+}
+
+/**
+ * 
+ * @TASK3 Ask CHAT-GPT to animate the transformation calculated in 
+ * task2 infinitely with a period of 10 seconds. 
+ * First 5 seconds, the cube should transform from its initial 
+ * position to the target position.
+ * The next 5 seconds, the cube should return to its initial position.
+ */
+//can.inanir
+// https://chat.openai.com/share/01a46527-c445-4154-9192-deddbc4e5bb5
+function getPeriodicMovement(startTime) {
+    // Get the current time
+    const currentTime = new Date().getTime();
+    
+    // Calculate the time elapsed since the animation started
+    const elapsedTime = (currentTime - startTime) % 10000; // 10 seconds interval
+    
+    // Define the transition duration
+    const transitionDuration = 5000; // 5 seconds
+    
+    // Create the initial transformation matrix
+    const initialMatrix = new Float32Array([
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1
+    ]);
+    
+    // Create the transformation matrix from task 2
+    const task2Matrix = new Float32Array([
+        0.17, -0.28, 0.73, 0.30,
+        0.30, 0.36, 0.28, -0.25,
+       -0.35,  0.17, 0.61, 0, 
+       0, 0, 0, 1
+    ]);
+    
+    // Check if we are in the first 5 seconds of the animation
+    if (elapsedTime <= transitionDuration) {
+        // Calculate the interpolation factor
+        const t = elapsedTime / transitionDuration;
+        
+        // Interpolate between the initial and task 2 matrices
+        const interpolatedMatrix = new Float32Array(16);
+        for (let i = 0; i < 16; i++) {
+            interpolatedMatrix[i] = (1 - t) * initialMatrix[i] + t * task2Matrix[i];
+        }
+        
+        // Return the interpolated matrix
+        return getTransposeMatrix(interpolatedMatrix);
+    } else {
+        // We are in the last 5 seconds of the animation, so reverse the transition
+        // Calculate the interpolation factor for the reverse transition
+        const t = (elapsedTime - transitionDuration) / transitionDuration;
+        
+        // Interpolate between the task 2 and initial matrices
+        const interpolatedMatrix = new Float32Array(16);
+        for (let i = 0; i < 16; i++) {
+            interpolatedMatrix[i] = (1 - t) * task2Matrix[i] + t * initialMatrix[i];
+        }
+        
+        // Return the interpolated matrix
+        return getTransposeMatrix(interpolatedMatrix);
+    }
+}
